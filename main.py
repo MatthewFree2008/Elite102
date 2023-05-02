45a443856330b9a470dcc677935bb551d849d0f9
Matthew Free <121990229+MatthewFree2008@users.noreply.github.com>
2023-05-01 19:26:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ cursor.execute(testQuery)
  cursor.close()
  connection.close()
 
-def balance():
-  balance = (sum)
+def balance():  
+    balance = (sum)
 
 
 def home_screen(): 
     print(f"Welcome {user_name}")
     print(f'balance =  {sum}')
+    user_selection()
 
 def deposit():
       deposit = input("insert value ") 
@@ -31,9 +32,10 @@ def withdraw():
 home_screen()
 
 def user_selection():
-  selection = int(input("Select a option, Deposit or Withdraw: "))
-  if selection == deposit or deposit:
-      deposit()
-  elif selection == withdraw or withdraw:
+  selection = int(input("Select a option, deposit or withdraw: "))
+  if selection == deposit:
+     print("Initiating Deposit")
+     deposit()
+  elif selection == withdraw:
+     print("Initiating Withdraw")
      withdraw()
-    #
